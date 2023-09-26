e6f4d38b4388c4f880ba384ef6e9629e1d2b21b9
kwakdeung <rhkrwnsdud22@naver.com>
2023-09-26 11:13:55 +0900

    가장 쉬운 Animation 코드 - 1
    *pubspec.yaml
    77 라인 외부 패키지 flutter_animate(애니메이션) 추가
    78 라인 외부 패키지 live_background(라이브 배경화면) 추가
    *cli_common.dart
    num_duration_extension.dart 파일 삭제로 인해 코드 지움
    *common.dart
    공통으로 쓰기 위한 import flutter_animate 추가
    *animation_controller_extension.dart
    flutter_animate 추가로 인해 common.dart를 import함.
    *num_duration_extension.dart 파일 삭제
    외부 패키지 flutter_animate 추가로 인해 불필요함
    *abs_theme_colors.dart
    51 라인 roundedLayoutBackground 투명도 조절
    *f_home.dart
    7 라인 외부 패키지 live_background의 palette import 추가
    8 라인 외부 패키지 live_background의 live_background_widget import 추가
    27~31 라인 LiveBackgroundWidget_Palette 추가
    *w_ttoss_app_bar.dart
    57~61 라인 flutter_animate 기능 추가
    *notifications_dummy.dart
    1 라인 num_duration_extension.dart 삭제 후 common.dart import 추가

--- flutter_1/fast_app_base/lib/screen/main/tab/home/f_home.dart
+++ flutter_1/fast_app_base/lib/screen/main/tab/home/f_home.dart
@@ -1,11 +1,11 @@
-import 'package:fast_app_base/common/cli_common.dart';
-
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:live_background/object/palette.dart';
+import 'package:live_background/widget/live_background_widget.dart';
 
 import '../../../../common/widget/w_big_button.dart';
 import '../../../dialog/d_color_bottom.dart';
@@ -24,6 +24,11 @@ class HomeFragment extends StatelessWidget {
       color: Colors.black,
       child: Stack(
         children: [
+          const LiveBackgroundWidget(
+            palette: Palette(colors: [Colors.red, Colors.green]),
+            velocityX: 1,
+            particleMaxSize: 20,
+          ),
           RefreshIndicator(
             edgeOffset: TtossAppBar.appBarHeight,
             onRefresh: () async {
@@ -52,7 +57,7 @@ class HomeFragment extends StatelessWidget {
                     ],
                   )),
                 ],
-              ).pSymmetric(h: 20),
+              ).pSymmetric(h: 20).animate().slideY(duration: 1000.ms).fadeIn(),
             ),
           ),
           const TtossAppBar(),
