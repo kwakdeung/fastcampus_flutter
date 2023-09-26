ab0901d22237e6668adbb6c85e8e8a2f0922a817
kwakdeung <rhkrwnsdud22@naver.com>
2023-09-26 13:37:09 +0900

    Flutter공식 Animation 지원 위젯 소개
    *w_ttoss_app_bar.dart
    15 라인 int _tappingCount 추가
    25~32 라인 주석 처리
    33~45 AnimatedCrossFade 위젯 추가 - 한 대상이 천천히 다른 대상으로 바뀌는 애니메이션 기능
    firstChild - 바뀌기 전 대상
    secondChild - 바뀐 후 대상
    crossFadeState - 바뀌게 하는 통제 기능
    duration - 시간
    47 라인 _tappingCount 텍스트화
    48 ~ 57 라인 Tap 위젯 기능 추가 후 setState에  _tappingCount++ 추가

--- flutter_1/fast_app_base/lib/screen/main/tab/home/w_ttoss_app_bar.dart
+++ flutter_1/fast_app_base/lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -12,6 +12,7 @@ class TtossAppBar extends StatefulWidget {
 
 class _TtossAppBarState extends State<TtossAppBar> {
   final bool _showRedDot = false;
+  int _tappingCount = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +22,39 @@ class _TtossAppBarState extends State<TtossAppBar> {
       child: Row(
         children: [
           width10,
-          Image.asset(
-            "$basePath/icon/toss.png",
-            height: 30,
-          ),
+          // AnimatedContainer(
+          //   duration: 1000.ms,
+          //   color: _tappingCount < 2 ? Colors.red : Colors.blue,
+          //   height: _tappingCount < 2 ? 60 : 30,
+          //   child: Image.asset(
+          //     "$basePath/icon/toss.png",
+          //   ),
+          // ),
+          AnimatedCrossFade(
+              firstChild: Image.asset(
+                "$basePath/icon/toss.png",
+                height: 30,
+              ),
+              secondChild: Image.asset(
+                "$basePath/icon/map_point.png",
+                height: 30,
+              ),
+              crossFadeState: _tappingCount < 2
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: 1500.ms),
           emptyExpanded,
-          Image.asset(
-            "$basePath/icon/map_point.png",
-            height: 30,
+          _tappingCount.text.make(),
+          Tap(
+            onTap: () {
+              setState(() {
+                _tappingCount++;
+              });
+            },
+            child: Image.asset(
+              "$basePath/icon/map_point.png",
+              height: 30,
+            ),
           ),
           width10,
           Tap(
