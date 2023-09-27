e71dc4d76bcacbdea3e98c7f82d0b4138bfc566a
kwakdeung <rhkrwnsdud22@naver.com>
2023-09-27 13:06:14 +0900

    Rive-Flutter 인터렉티브 애니메이션 구현해보기
    *pubspec.yaml
    80 라인 외부 패키지 rive 추가
    *rive 움직이는 이미지 파일 샘플 추가
    light_like.riv, light_like2.riv, login_screen_character.riv
    *constants.dart
    path 상수 baseRivePath 추가
    *f_home.dart
    17~26 라인 StatelessWidget -> StatefulWidget 변경
    27 라인 isLike bool 상수 추가
    51~61 라인 RiveLikeButton 위젯 추가
    75~77 라인 코드 정리
    82 라인 미사용 애니메이션 주석 달기
    *w_rive_like_button.dart
    RiveLikeButton class 위젯 추가
    *d_number.dart
    20~21 라인 numberController, passwordController 세분화
    22~23 라인 numberFocus, passwordFocus 세분화
    24 라인 TextWatchingBearController 상수 textBearController 추가
    25~27 라인 상수 check, handsUp, look 추가
    29~47 라인 initState() 함수 추가
    numberController, numberFocus, passwordFocus의 addListener 추가
    51 라인 Material -> Scaffold 위젯 변경
    52 라인 backgroundColor 투명색으로 변경
    53 라인 Scaffold 위젯 변경으로 인해 body로 변경
    60~69 라인 TextWatchingBear rive 이미지 위젯 추가
    70~74 라인 number TextField 위젯 추가
    75~80 라인 password TextField 위젯 추가
    83, 88 라인 비동기 async await 추가
    84 라인 text controller 추가
    85~92 라인 try~catch 문
    예외가 아닐 경우 textBearController.runSuccessAnimation
    예외일 경우 textBearController.runFailAnimation
    *w_text_watching_bear.dart
    TextWatchingBear class 위젯 추가
    5~8 라인 TextWatchingBearController class(Animation - Success, Fail) 추가

--- flutter_1/fast_app_base/lib/screen/main/tab/home/f_home.dart
+++ flutter_1/fast_app_base/lib/screen/main/tab/home/f_home.dart
@@ -2,6 +2,7 @@ import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
+import 'package:fast_app_base/screen/main/tab/home/w_rive_like_button.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttoss_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:live_background/object/palette.dart';
@@ -13,10 +14,17 @@ import '../../../dialog/d_confirm.dart';
 import '../../s_main.dart';
 import 'bank_accounts_dummy.dart';
 
-class HomeFragment extends StatelessWidget {
+class HomeFragment extends StatefulWidget {
   const HomeFragment({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<HomeFragment> createState() => _HomeFragmentState();
+}
+
+class _HomeFragmentState extends State<HomeFragment> {
+  bool isLike = false;
 
   @override
   Widget build(BuildContext context) {
@@ -35,30 +43,44 @@ class HomeFragment extends StatelessWidget {
               await sleepAsync(500.ms);
             },
             child: SingleChildScrollView(
-              padding: const EdgeInsets.only(
-                  top: TtossAppBar.appBarHeight + 10,
-                  bottom: MainScreenState.bottomNavigatorHeight),
-              child: Column(
-                children: [
-                  BigButton(
-                    text: "토스뱅크",
-                    onTap: () {
-                      context.showSnackbar("토스뱅크를 눌렀어요.");
-                    },
-                  ),
-                  height10,
-                  RoundedContainer(
-                      child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      "자산".text.bold.white.make(),
-                      height5,
-                      ...bankAccounts.map((e) => BankAccountWidget(e)).toList()
-                    ],
-                  )),
-                ],
-              ).pSymmetric(h: 20).animate().slideY(duration: 1000.ms).fadeIn(),
-            ),
+                padding: const EdgeInsets.only(
+                    top: TtossAppBar.appBarHeight + 10,
+                    bottom: MainScreenState.bottomNavigatorHeight),
+                child: Column(
+                  children: [
+                    SizedBox(
+                        height: 250,
+                        width: 250,
+                        child: RiveLikeButton(
+                          isLike,
+                          onTapLike: (bool isLike) {
+                            setState(() {
+                              this.isLike = isLike;
+                            });
+                          },
+                        )),
+                    BigButton(
+                      text: "토스뱅크",
+                      onTap: () {
+                        context.showSnackbar("토스뱅크를 눌렀어요.");
+                      },
+                    ),
+                    height10,
+                    RoundedContainer(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        "자산".text.bold.white.make(),
+                        height5,
+                        ...bankAccounts
+                            .map((e) => BankAccountWidget(e))
+                            .toList()
+                      ],
+                    )),
+                  ],
+                )
+                // .pSymmetric(h: 20).animate().slideY(duration: 1000.ms).fadeIn(),
+                ),
           ),
           const TtossAppBar(),
         ],
