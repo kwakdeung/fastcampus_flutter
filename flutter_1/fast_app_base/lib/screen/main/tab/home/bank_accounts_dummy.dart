87f30d49aaff1e272d5afd123bfc6709c9cfaa05
kwakdeung <rhkrwnsdud22@naver.com>
2023-10-04 11:38:00 +0900

    Intro. Collections (List, Set, Map), Generic
    *collection_extension.dart
    extension ListExtension와 swap 메서드, 스트림 추가
    *bank_accounts_dummy.dart
    1 라인 import common.dart 추가
    6 라인 가격 변경 3000000 -> 300
    8 라인 가격 변경 30000000 -> 200
    10 라인 가격 변경 300000000 -> 100
    11 라인 가격 변경 5000000 -> 400
    14 라인 bankAccountKakao2 카카오 특별통장 추가
    17 라인 abstract class Animal - eat 추가
    21 라인 Animal 상속 Dog class 추가
    27 라인 Animal 상속 Cat class 추가
    33 라인 Animal 상속 Cow class 추가
    40~41 라인 삽입
    43~45 라인 위치 이동
    47~48 라인 교환
    50~57 라인 변환
    59~66 라인 생성
    68~71 라인 class generic
    73~76 라인 method or function generic
    79~115 라인 Result, ResultString, ResultDouble class 추가
    doTheWork, doTheWork2, doTheWork3 함수 추가
    *vo_bank.dart
    1, 3 라인 @freezed
    10~13 라인 toString() 함수 추가
    15~24 라인 bool operator == 추가

--- flutter_1/fast_app_base/lib/screen/main/tab/home/bank_accounts_dummy.dart
+++ flutter_1/fast_app_base/lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -1,39 +1,121 @@
+import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/screen/main/tab/home/banks_dummy.dart';
 import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
 
 final bankAccountShinhan1 =
-    BankAccount(bankShinhan, 3000000, accountTypeName: "신한 주거래 우대통장(저축예금)");
+    BankAccount(bankShinhan, 300, accountTypeName: "신한 주거래 우대통장(저축예금)");
 final bankAccountShinhan2 =
-    BankAccount(bankShinhan, 30000000, accountTypeName: "저축예금");
+    BankAccount(bankShinhan, 200, accountTypeName: "저축예금");
 final bankAccountShinhan3 =
-    BankAccount(bankShinhan, 300000000, accountTypeName: "저축예금");
-final bankAccountToss = BankAccount(bankTtoss, 5000000);
+    BankAccount(bankShinhan, 100, accountTypeName: "저축예금");
+final bankAccountToss = BankAccount(bankTtoss, 400);
 final bankAccountKakao =
-    BankAccount(bankKakao, 7000000, accountTypeName: "입출금통장");
+    BankAccount(bankKakao, 70000, accountTypeName: "입출금통장");
+final bankAccountKakao2 =
+    BankAccount(bankKakao, 1000000, accountTypeName: "특별통장");
+
+abstract class Animal {
+  void eat();
+}
+
+class Dog extends Animal {
+  void eat() {
+    print('dog');
+  }
+}
+
+class Cat extends Animal {
+  void eat() {
+    print('cat');
+  }
+}
+
+class Cow extends Animal {
+  void eat() {
+    print('cow');
+  }
+}
 
 main() {
-  final shinhanBank = bankMap["shinhan1"];
-  print(shinhanBank == bankAccountShinhan1);
+  // 삽입
+  bankAccounts.insert(1, bankAccountKakao2);
 
-  for (final entry in bankMap.entries) {
-    print(entry.key +
-        ":" +
-        (entry.value.accountTypeName ?? entry.value.bank.name));
+  // 위치 이동
+  final temp = bankAccounts.removeAt(4);
+  bankAccounts.insert(0, temp);
+
+  // 교환
+  bankAccounts.swap(0, 5);
+
+  // 변환
+  final banks = bankAccounts.map((e) => e.bank).toList();
+
+  if (banks[3] != banks[5]) {}
+  final uniqueBanks = banks.toSet();
+  for (final bank in uniqueBanks) {
+    print(bank.toString());
   }
 
-  bankSet.contains(bankAccountShinhan1);
+  // 생성
+  final list = [];
 
-  print(bankAccounts.contains(bankAccountShinhan1));
+  final set = {bankAccountShinhan1, bankAccountShinhan2};
+  final map = {
+    "shinhan1": bankAccountShinhan1,
+    "shinhan2": bankAccountShinhan2
+  };
+
+  // class generic
+  final result = doTheWork();
+  print(result.data);
+  final result2 = doTheWork2();
+
+  // method or function generic
+
+  final result3 = doTheWork3<Dog>(() => Dog());
+  result3.eat();
+}
+
+class Result<T> {
+  final T data;
+
+  Result(this.data);
+}
+
+class ResultString {
+  final String data;
+
+  ResultString(this.data);
+}
+
+class ResultDouble {
+  final double data;
+
+  ResultDouble(this.data);
+}
+
+Result<String> doTheWork() {
+  ///...1
+  ///...2
+  ///
+  ///...4
+  return Result("중요한 데이터");
+}
+
+ResultDouble doTheWork2() {
+  ///...1
+  ///...2
+  ///
+  ///...4
+  return ResultDouble(5234.44);
+}
+
+Result doTheWork3<Result extends Animal>(Result Function() animalCreator) {
+  return animalCreator();
 }
 
 //List
 final bankAccounts = [
-  bankAccountShinhan1,
-  bankAccountShinhan1,
-  bankAccountShinhan1,
-  bankAccountShinhan1,
-  bankAccountShinhan1,
-  bankAccountShinhan1,
   bankAccountShinhan1,
   bankAccountShinhan2,
   bankAccountShinhan3,
