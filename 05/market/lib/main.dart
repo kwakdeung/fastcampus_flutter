30513238d7472ec0cd06c23e2cde60ace6701d33
kwakdeung <rhkrwnsdud22@naver.com>
2024-01-25 17:23:35 +0900

    05_market_배달 앱 데이터 모델 구현

--- 05/market/lib/main.dart
+++ 05/market/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:market/home/cart_screen.dart';
 import 'package:market/home/home_screen.dart';
-import 'package:market/home/product_add_screen.dart';
 import 'package:market/home/product_detail_screen.dart';
 import 'package:market/login/login_screen.dart';
 import 'package:market/login/sign_up_screen.dart';
