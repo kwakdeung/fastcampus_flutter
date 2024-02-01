4e0cc416c2ab3376636a9e2a550cad2c8ed74542
kwakdeung <rhkrwnsdud22@naver.com>
2024-02-01 17:17:42 +0900

    05_realtime quiz app_(Web) 퀴즈 출제자 앱 구현 - 퀴즈 출제 4

--- 05/realtime_quiz_app/lib/web/quiz_manager_page.dart
+++ 05/realtime_quiz_app/lib/web/quiz_manager_page.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:realtime_quiz_app/main.dart';
 import 'package:realtime_quiz_app/model/quiz.dart';
 import 'package:realtime_quiz_app/web/quiz_bottom_sheet_widget.dart';
 
@@ -23,6 +26,50 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
       setState(() {
         uid = value.user?.uid ?? "";
       });
+    });
+  }
+
+  generateQuiz() async {
+    if (quizItems.isEmpty) {
+      return;
+    }
+    final pinCode = Random().nextInt(999999).toString().padLeft(6);
+    final quizRef = database!.ref("quiz");
+    final quizDetailRef = database!.ref("quiz_detail");
+    final quizStateRef = database!.ref("quiz_state");
+
+    final newQuizDetailRef = quizDetailRef.push();
+    newQuizDetailRef.set({
+      "code": pinCode,
+      "problem": quizItems
+          .map(
+            (e) => {
+              "title": e.title,
+              "options": e.problems
+                  ?.map((e2) => e2.textEditingController.text)
+                  .toList(),
+              "answerIndex": e.answer?.index,
+              "answer": e.answer?.textEditingController.text
+            },
+          )
+          .toList(),
+    });
+
+    await quizStateRef.child("${newQuizDetailRef.key}").set({
+      "quizDetailRef": newQuizDetailRef.key,
+      "user": [],
+      "state": false,
+      "score": [],
+      "solve": [{}],
+    });
+
+    final newQuizRef = quizRef.push();
+    await newQuizRef.set({
+      "code": pinCode,
+      "uid": uid,
+      "generateTime": DateTime.now().toString(),
+      "timestamp": DateTime.now().millisecondsSinceEpoch,
+      "quizDetailRef": newQuizDetailRef.key,
     });
   }
 
@@ -86,6 +133,7 @@ class _QuizManagerPageState extends State<QuizManagerPage> {
                       MaterialButton(
                         onPressed: () {
                           /// TODO: 퀴즈 생성 및 핀코드 생성 로직 추가
+                          generateQuiz();
                         },
                         height: 72,
                         color: Colors.indigo,
