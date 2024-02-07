aeb702dcd97e0ce29e4deba16ac8819e92264195
kwakdeung <rhkrwnsdud22@naver.com>
2024-02-07 15:08:25 +0900

    05_flutter mlkit sandbox_프로젝트 셋팅

---
+++ 05/flutter_mlkit_sandbox/android/app/src/main/kotlin/com/example/flutter_mlkit_sandbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_mlkit_sandbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 05/flutter_mlkit_sandbox/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
