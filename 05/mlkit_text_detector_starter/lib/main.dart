577eb65e08b77999af0e14b42945dcd9f5ba9087
kwakdeung <rhkrwnsdud22@naver.com>
2024-02-08 11:14:33 +0900

    05_mlkit text detector starter_인식 결과 화면 구현

--- 05/mlkit_text_detector_starter/lib/main.dart
+++ 05/mlkit_text_detector_starter/lib/main.dart
@@ -37,8 +37,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  bool _canProcess = true;
+  bool _isBusy = false;
+  String _text = "";
+  CustomPaint? _customPaint;
+  final _cameraLensDirection = CameraLensDirection.back;
+
+  var _script = TextRecognitionScript.korean;
+  TextRecognizer textRecognizer =
+      TextRecognizer(script: TextRecognitionScript.korean);
+  ImagePicker _imagePicker = ImagePicker();
+
   @override
   void dispose() {
+    _canProcess = false;
+    textRecognizer.close();
     super.dispose();
   }
 
@@ -49,7 +63,58 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: buildGalleryView(),
+      body: Column(
+        children: [
+          DropdownButton(
+            items: TextRecognitionScript.values
+                .map(
+                  (e) => DropdownMenuItem(
+                    child: Text(e.name),
+                    value: e,
+                  ),
+                )
+                .toList(),
+            value: _script,
+            onChanged: (script) {
+              setState(() {
+                if (script != null) {
+                  setState(() {
+                    _script = script!;
+                    textRecognizer.close();
+                    textRecognizer = TextRecognizer(script: _script);
+                  });
+                }
+              });
+            },
+          ),
+          _image != null
+              ? SizedBox(
+                  height: 320,
+                  width: 320,
+                  child: Image.file(
+                    _image!,
+                  ),
+                )
+              : Container(
+                  height: 240,
+                  width: 240,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                ),
+          ElevatedButton(
+            onPressed: () => _getImage(ImageSource.gallery),
+            child: Text("갤러리 이미지 가져오기"),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Text(
+                _text,
+              ),
+            ),
+          ),
+        ],
+      ),
       // body: CameraView(
       //   customPaint: _customPaint,
       //   onImage: _processImage,
@@ -61,9 +126,53 @@ class _MyHomePageState extends State<MyHomePage> {
         children: [],
       );
 
-  Future _getImage(ImageSource source) async {}
+  Future _getImage(ImageSource source) async {
+    setState(() {
+      _image = null;
+    });
+    final pickedFile = await _imagePicker.pickImage(source: source);
+    if (pickedFile != null) {
+      _processFile(pickedFile.path);
+    }
+  }
 
-  Future _processFile(String path) async {}
+  Future _processFile(String path) async {
+    setState(() {
+      _image = File(path);
+    });
+    final inputImage = InputImage.fromFilePath(path);
+    _processImage(inputImage);
+  }
 
-  Future<void> _processImage(InputImage inputImage) async {}
+  Future<void> _processImage(InputImage inputImage) async {
+    if (!_canProcess) return;
+    if (_isBusy) return;
+    _isBusy = true;
+    setState(() {
+      _text = '';
+    });
+    final recognizedText = await textRecognizer.processImage(inputImage);
+    if (inputImage.metadata?.size != null &&
+        inputImage.metadata?.rotation != null) {
+    } else {
+      // 갤러리에서 가져온 값 처리
+      _text = "recognized Text: ${recognizedText.text} \n\n";
+      for (var block in recognizedText.blocks) {
+        _text += "Block: ${block.text}\n";
+        for (var line in block.lines) {
+          _text += "Line: ${line.text}\n";
+          for (var element in line.elements) {
+            _text += "element: ${element.text}\n";
+          }
+        }
+        _text += "\n\n";
+      }
+      setState(() {});
+      _customPaint = null;
+    }
+    _isBusy = false;
+    if (mounted) {
+      setState(() {});
+    }
+  }
 }
