b1b87ea8cb154b535ebf7e2343422654df8b1c42
kwakdeung <rhkrwnsdud22@naver.com>
2024-02-16 17:35:58 +0900

    06_e commerce app_GnbBar 개발 - Data Layer(ResponseWrapper 개발)

--- 06/e_commerce_app/lib/data/data_source/remote/display.api.dart
+++ 06/e_commerce_app/lib/data/data_source/remote/display.api.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:retrofit/retrofit.dart';
 
+import '../../dto/common/response_wrapper/response_wrapper.dart';
 import '../../dto/display/menu/menu.dto.dart';
 
 part 'display.api.g.dart';
@@ -11,5 +12,7 @@ abstract class DisplayApi {
   factory DisplayApi(Dio dio, {String baseUrl}) = _DisplayApi;
 
   @GET('/api/menus/{mall_type}')
-  Future<List<MenuDto>> getMenusByMallType(@Path('mall_type') String mallType);
+  Future<ResponseWrapper<List<MenuDto>>> getMenusByMallType(
+    @Path('mall_type') String mallType,
+  );
 }
--- 06/e_commerce_app/lib/main.dart
+++ 06/e_commerce_app/lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import 'core/theme_data.dart';
+import 'data/data_source/mock/display/display_mock_api.dart';
 import 'presentation/pages/home/home_page.dart';
 import 'presentation/pages/splash/splash_page.dart';
 
-void main() {
+void main() async {
+  final data = await DisplayMockApi().getMenusByMallType('market');
+  print(data);
   runApp(const MainApp());
 }
 
