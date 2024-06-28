f9ca2ad6711d98dca386c8eeabea343578a1e97d
kwakdeung <rhkrwnsdud22@naver.com>
2024-06-28 10:27:17 +0900

    상태관리 라이브러리 BLoC 01. 실습 - BLoC (1)

---
+++ bloc/bloc_stopwatch_exam/README.md
@@ -0,0 +1,3 @@
+상태관리 라이브러리 BLoC 01. 실습 - BLoC (1)
+
+# bloc_stopwatch_exam
---
+++ bloc/bloc_stopwatch_exam/lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:bloc_stopwatch_exam/bloc/stopwatch_bloc.dart';
+import 'package:bloc_stopwatch_exam/stopwatch_screen.dart';
+import 'package:bloc_stopwatch_exam/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<StopwatchBloc>(
+      create: (context) => StopwatchBloc(),
+      child: MaterialApp(
+        title: 'Bloc Stopwatch 예제',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const StopwatchScreen(),
+      ),
+    );
+  }
+}
